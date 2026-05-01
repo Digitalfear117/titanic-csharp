67f4333326c056e380b0f9d72ddcac74c0da7390
Lekuru <contact@lekuru.xyz>
2026-05-01 16:02:57 +0200

    Streamline test formatting

--- Titanic.API.Tests/BenchmarkRequestTests.cs
+++ Titanic.API.Tests/BenchmarkRequestTests.cs
@@ -1,35 +1,39 @@
 using Titanic.API.Requests;
 
-namespace Titanic.API.Tests;
-
-public class BenchmarkRequestTests : TitanicAPITest
+namespace Titanic.API.Tests
 {
-    [Fact]
-    public void GetBenchmarkScores_HasValidBenchmarkProperties()
+    /// <summary>
+    /// Tests for benchmark-related API requests.
+    /// </summary>
+    public class BenchmarkRequestTests : TitanicAPITest
     {
-        // Arrange
-        var request = new BenchmarkScoresRequest();
-        var benchmarks = request.BlockingPerform(Api);
+        [Fact]
+        public void GetBenchmarkScores_HasValidBenchmarkProperties()
+        {
+            // Arrange
+            var request = new BenchmarkScoresRequest();
+            var benchmarks = request.BlockingPerform(Api);
 
-        // Act
-        var benchmark = benchmarks[0];
+            // Act
+            var benchmark = benchmarks[0];
 
-        // Assert
-        Assert.True(benchmark.Id > 0);
-        Assert.True(benchmark.Score > 0);
-        Assert.True(benchmark.Framerate > 0);
-        Assert.NotNull(benchmark.Grade);
-        Assert.NotNull(benchmark.Client);
-    }
+            // Assert
+            Assert.True(benchmark.Id > 0);
+            Assert.True(benchmark.Score > 0);
+            Assert.True(benchmark.Framerate > 0);
+            Assert.NotNull(benchmark.Grade);
+            Assert.NotNull(benchmark.Client);
+        }
 
-    [Fact]
-    public void GetBenchmarkScores_WithPagination_ReturnsData()
-    {
-        // Arrange & Act
-        var request = new BenchmarkScoresRequest(page: 2);
-        var benchmarks = request.BlockingPerform(Api);
+        [Fact]
+        public void GetBenchmarkScores_WithPagination_ReturnsData()
+        {
+            // Arrange & Act
+            var request = new BenchmarkScoresRequest(page: 2);
+            var benchmarks = request.BlockingPerform(Api);
 
-        // Assert
-        Assert.NotNull(benchmarks);
+            // Assert
+            Assert.NotNull(benchmarks);
+        }
     }
 }

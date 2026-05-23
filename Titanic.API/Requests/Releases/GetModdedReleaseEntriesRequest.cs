d5c593ce51580d41647d3fda0a7323322a1de562
Lekuru <contact@lekuru.xyz>
2026-05-23 15:33:11 +0200

    Add optional `version` parameter

--- Titanic.API/Requests/Releases/GetModdedReleaseEntriesRequest.cs
+++ Titanic.API/Requests/Releases/GetModdedReleaseEntriesRequest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Titanic.API.Models;
 
@@ -6,19 +7,30 @@ namespace Titanic.API.Requests
     public class GetModdedReleaseEntriesRequest : APIRequest<List<ModdedReleaseEntryModel>>
     {
         public string Identifier { get; set; }
+        public string Version { get; set; }
         public int Offset { get; set; }
         public int Limit { get; set; }
 
-        public GetModdedReleaseEntriesRequest(string identifier, int offset = 0, int limit = 10)
+        public GetModdedReleaseEntriesRequest(string identifier, int offset = 0, int limit = 10, string version = null)
         {
             Identifier = identifier;
+            Version = version;
             Offset = offset;
             Limit = limit;
         }
 
         protected override List<ModdedReleaseEntryModel> Execute(TitanicAPI api)
         {
-            return api.GetList<ModdedReleaseEntryModel>($"/releases/modded/{Identifier}/entries?offset={Offset}&limit={Limit}");
+            List<string> query = new List<string>
+            {
+                $"offset={Offset}",
+                $"limit={Limit}"
+            };
+
+            if (!string.IsNullOrEmpty(Version))
+                query.Add($"version={Uri.EscapeDataString(Version)}");
+
+            return api.GetList<ModdedReleaseEntryModel>($"/releases/modded/{Identifier}/entries?{string.Join("&", query.ToArray())}");
         }
     }
 }
